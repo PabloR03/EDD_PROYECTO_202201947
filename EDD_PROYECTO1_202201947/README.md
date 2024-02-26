406df87170a6d1c06e081ca536829cb7693c93c4
Pablo Rodríguez Lima <pabloa10rodriguez@gmail.com>
2024-02-25 21:57:29 -0600

    archivo de prueba porque la carpeta .git me da error

---
+++ EDD_PROYECTO1_202201947/README.md
@@ -0,0 +1,2 @@
+# EDD_PROYECTO1_202201947
+My cool new project!
---
+++ EDD_PROYECTO1_202201947/src/EDD_PROYECTO1_202201947.f90
@@ -0,0 +1,10 @@
+module EDD_PROYECTO1_202201947
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, EDD_PROYECTO1_202201947!"
+  end subroutine say_hello
+end module EDD_PROYECTO1_202201947
