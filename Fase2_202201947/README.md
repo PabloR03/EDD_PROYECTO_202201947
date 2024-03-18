851a180f097014e74faaa920666873e2b5741938
Pablo Rodríguez Lima <pabloa10rodriguez@gmail.com>
2024-03-17 20:41:08 -0600

    A leer Json por lo menos

---
+++ Fase2_202201947/README.md
@@ -0,0 +1,2 @@
+# Fase2_202201947
+My cool new project!
---
+++ Fase2_202201947/src/Fase2_202201947.f90
@@ -0,0 +1,10 @@
+module Fase2_202201947
+  implicit none
+  private
+
+  public :: say_hello
+contains
+  subroutine say_hello
+    print *, "Hello, Fase2_202201947!"
+  end subroutine say_hello
+end module Fase2_202201947
